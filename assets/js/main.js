c01139df35d9f0505ec0b061d4e2a57f2594ef35
Jonathon Shields <jonathonshields@gmail.com>
2016-06-17 08:42:02 +1200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,7 +57,7 @@ var parallelism = (function($) { var _ = {
 						maxRows: 3,
 
 					// Minimum number of rows.
-						minRows: 1,
+						minRows: 2,
 
 					// Padding (in px) between items (0 = no padding).
 						padding: 5,
